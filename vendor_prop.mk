8c9dcf140263a93f6d1d6baabaf78f86bd55ea51
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    oce: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -114,7 +114,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.recomputecrop=0 \
     vendor.display.disable_rotator_downscale=1 \
     ro.sf.lcd_density=640 \
-    vendor.display.disable_skip_validate=1
+    vendor.display.disable_skip_validate=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
