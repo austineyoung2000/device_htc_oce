d7ef54ec5de75f5c6f1777f2a39c155987f5deca
TARKZiM <tom8476oo@gmail.com>
2022-02-11 10:28:05 +0800

    oce: Switch to Audio HAL V6

--- device.mk
+++ device.mk
@@ -117,9 +117,9 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
